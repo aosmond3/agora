1100193de324be910be1cf37795a811b2c4b05eb
Andrew Osmond <aosmond3@gatech.edu>
2023-10-20 16:17:28 -0400

    Initial project structure

---
+++ src/pages/LoginPage/Login.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import axios from 'axios';
+import './Login.css';
+
+axios.defaults.baseURL = "http://localhost:8000";
+
+function Login () {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleUsernameInput = (e) => setUsername(e.target.value);
+    const handlePasswordInput = (e) => setPassword(e.target.value);
+
+
+    const handleLogIn = (e) => {
+        e.preventDefault(); // prevents the form from being submitted (i.e. the page from rerendering and state being lost)
+
+        axios({
+            method: "GET",
+            url:"/api/users",
+        })
+        .then((response) => {
+            const res = response.data;
+            console.log(res);
+        }).catch((error) => {
+        if (error.response) {
+            console.log(error.response);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            }
+        });
+    }
+
+    return (
+        <div className='login-container'>
+            <h1>AGORA</h1>
+            <form onSubmit={handleLogIn}>
+                <label htmlFor='username'>Username:</label>
+                <input type='text' id='username' autoComplete='off' onChange={handleUsernameInput}></input>
+                <label htmlFor='password'>Password:</label>
+                <input type='text' id='password' autoComplete='off' onChange={handlePasswordInput}></input>
+                <button type='submit'>Log in</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
